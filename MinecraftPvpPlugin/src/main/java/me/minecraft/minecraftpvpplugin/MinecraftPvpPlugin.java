ece121781ff2942ece86d0c828ad19bbb1c024a6
unknown <just1213666@gmail.com>
2024-01-21 23:30:49 +0800

    待測試

--- MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/MinecraftPvpPlugin.java
+++ MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/MinecraftPvpPlugin.java
@@ -157,6 +157,7 @@ public final class MinecraftPvpPlugin extends JavaPlugin implements Listener{
             player.getInventory().setItem(2, Gadgets.Invisible);
             player.getInventory().setItem(3, Gadgets.Damage);
             player.getInventory().setItem(4, Gadgets.Speed);
+            player.getInventory().setItem(5, Gadgets.Rebound);
             player.getInventory().setItem(8, Items.Gapple);
             player.setHealth(20);
             player.setFoodLevel(20);
