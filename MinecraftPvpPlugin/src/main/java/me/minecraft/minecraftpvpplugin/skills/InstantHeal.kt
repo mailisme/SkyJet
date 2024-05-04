56e608115f4f1fef3615d8ded5be05d6bfce26e8
mailisme <just1213666@gmail.com>
2024-05-04 15:37:12 +0800

    sdgfdfgs

--- MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/skills/InstantHeal.kt
+++ MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/skills/InstantHeal.kt
@@ -4,6 +4,7 @@ import me.minecraft.minecraftpvpplugin.MinecraftPvpPlugin
 import me.minecraft.minecraftpvpplugin.Skill
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.entity.Entity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
@@ -11,14 +12,23 @@ import org.bukkit.inventory.ItemStack
 import kotlin.random.Random
 
 object InstantHeal : Skill(Material.EMERALD, "瞬間治癒") {
+
     public override fun onClick(event: PlayerInteractEvent) {
         val player = event.player
         val rand = Random.nextInt(5, 10)
-        if (player.health + rand > 20) {
-            player.health -= (player.health + rand - 20)
+        createCoolDown.create("InstantHeal")
+        val coolDown = createCoolDown.coolDowns[0]
+
+        if (System.currentTimeMillis()/10 > coolDown!!) {
+            if (player.health + rand > 20) {
+                player.health -= (player.health + rand - 20)
+            }
+            else {
+                player.health += rand
+            }
         }
         else {
-            player.health += rand
+            player.sendMessage("再等" + (coolDown - System.currentTimeMillis()/10) + "秒")
         }
     }
 }
