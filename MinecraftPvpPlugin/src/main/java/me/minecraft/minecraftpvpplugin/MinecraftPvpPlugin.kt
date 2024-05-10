c56dc53b6aa6a84a2a7a1f5e2ec3a852ee2f6c17
ELEPOT <elepotmail0@gmail.com>
2024-05-10 21:01:03 +0800

    test 5

--- MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/MinecraftPvpPlugin.kt
+++ MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/MinecraftPvpPlugin.kt
@@ -80,7 +80,7 @@ class MinecraftPvpPlugin : JavaPlugin(), Listener {
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
 
-        event.joinMessage = "${ChatColor.AQUA}Welcome ${player.name}!!!! LALALALA no.. no again.."
+        event.joinMessage = "${ChatColor.AQUA}Welcome ${player.name}!!!! LALALALA no.. no again.. bad"
         logger.info("${player.name} joined the server")
         player.teleport(Locations.lobbySpawn)
         onPlayerToLobby(player)
