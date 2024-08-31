5dca14106b70ab6c8990c80fe4d8cea0d497c1e3
mailisme <just1213666@gmail.com>
2024-09-01 01:07:19 +0800

    sdfg

--- MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/bot/SpawnBot.kt
+++ MinecraftPvpPlugin/src/main/java/me/minecraft/minecraftpvpplugin/bot/SpawnBot.kt
@@ -1,8 +1,11 @@
 package me.minecraft.minecraftpvpplugin.bot
 
 import com.mojang.authlib.GameProfile
+import me.minecraft.minecraftpvpplugin.refs.Items
 import net.minecraft.server.v1_8_R3.*
+import org.bukkit.Material
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
+import org.bukkit.craftbukkit.v1_8_R3.util.CraftMagicNumbers
 import org.bukkit.entity.Player
 import org.bukkit.inventory.EquipmentSlot
 import java.util.*
@@ -26,6 +29,8 @@ object SpawnBot {
 
         ps.sendPacket(PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, bot))
         ps.sendPacket(PacketPlayOutNamedEntitySpawn(bot))
-        ps.sendPacket(PacketPlayOutEntityEquipment(bot.bukkitEntity.entityId, List(1, EquipmentSlot.H)))
+        ps.sendPacket(PacketPlayOutEntityEquipment(bot.bukkitEntity.entityId, 1, ItemStack(CraftMagicNumbers.getItem(Material.IRON_SWORD))))
+
+
     }
 }
